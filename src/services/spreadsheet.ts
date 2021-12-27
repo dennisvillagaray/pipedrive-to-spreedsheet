4447d99f431ca91ee7aa09784607df795d2f227e
dennisvillagaray <dennis.villagarayg@gmail.com>
2021-12-26 20:16:14 -0500

    adding moment.js

--- src/services/spreadsheet.ts
+++ src/services/spreadsheet.ts
@@ -1,11 +1,11 @@
 import { accessSpreadSheet } from "../config";
+import moment from 'moment';
 
 export const addRowIntoGoogleSheet = async (params: any) => {
   const doc = await accessSpreadSheet()
   await doc.loadInfo();
 
-  const date = new Date()
-  const curretDay = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  const currentTime = moment().format('DD/MM/YYYY HH:MM:SS')
 
   const sheet = doc.sheetsByIndex[2];
   await sheet.addRow({
@@ -16,6 +16,6 @@ export const addRowIntoGoogleSheet = async (params: any) => {
     'producto Culqi': params.product,
     utm_source: params.utm_source,
     origin: params.origin,
-    'Fecha-Hora Creado': curretDay,
+    'Fecha-Hora Creado': currentTime,
   });
 }
